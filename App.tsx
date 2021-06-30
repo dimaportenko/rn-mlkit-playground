6540d832cd2e14c919df549ccef7116363aa3a07
Dmytro Portenko <troublesohard@gmail.com>
2021-06-30 22:41:07 +0300

    pick image template

---
+++ App.tsx
@@ -0,0 +1,83 @@
+/**
+ * Created by Dima Portenko on 30.06.2021
+ */
+import React from 'react';
+import {
+  View,
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+} from 'react-native';
+import {DemoButton, DemoResponse} from './src/components';
+import * as ImagePicker from 'react-native-image-picker';
+import {ImagePickerResponse} from 'react-native-image-picker/src/types';
+
+export const App = () => {
+  const {width} = useWindowDimensions();
+  const [response, setResponse] = React.useState<ImagePickerResponse | null>(
+    null,
+  );
+
+  const onButtonPress = React.useCallback((type, options) => {
+    if (type === 'capture') {
+      ImagePicker.launchCamera(options, setResponse);
+    } else {
+      ImagePicker.launchImageLibrary(options, setResponse);
+    }
+  }, []);
+
+  return (
+    <View style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1, flexDirection: 'column-reverse'}}>
+        <View style={{flexDirection: 'row', paddingVertical: 16}}>
+          <DemoButton
+            key="Take Image"
+            onPress={() =>
+              onButtonPress('capture', {
+                saveToPhotos: true,
+                mediaType: 'photo',
+                includeBase64: false,
+              })
+            }>
+            {'Take Image'}
+          </DemoButton>
+          <DemoButton
+            key="Select Image"
+            onPress={() =>
+              onButtonPress('library', {
+                // maxHeight: 200,
+                // maxWidth: 200,
+                selectionLimit: 0,
+                mediaType: 'photo',
+                includeBase64: false,
+              })
+            }>
+            {'Select Image'}
+          </DemoButton>
+        </View>
+        <View style={{paddingHorizontal: 8}}>
+          <DemoResponse>{response}</DemoResponse>
+        </View>
+        {response?.assets &&
+          response?.assets.map(({uri}) => (
+            <View key={uri} style={styles.image}>
+              <Image
+                resizeMode="cover"
+                resizeMethod="scale"
+                style={{width, height: width}}
+                source={{uri: uri}}
+              />
+            </View>
+          ))}
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    marginVertical: 24,
+    alignItems: 'center',
+  },
+});
